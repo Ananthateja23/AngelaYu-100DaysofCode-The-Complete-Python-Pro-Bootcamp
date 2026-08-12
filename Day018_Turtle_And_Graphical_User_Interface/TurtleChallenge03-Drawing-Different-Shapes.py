b2e1bce269203456f56ee4bb294d09c05b87813d
AnanthaTeja23 <thatha.ananthateja@gmail.com>
2026-08-12 22:15:09 +0530

    Add TurtleChallenge03 Drawing Different Shapes

---
+++ Day018_Turtle_And_Graphical_User_Interface/TurtleChallenge03-Drawing-Different-Shapes.py
@@ -0,0 +1,43 @@
+import turtle as t
+import random
+
+genie = t.Turtle()
+
+# search "turtle colors python"
+# go to(click on) "colors Trinket"
+# select the colors
+
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for side in range(num_sides):
+        genie.forward(100)
+        genie.right(angle)
+
+for shape_side_n in range(3, 11):
+    genie.color(random.choice(colors))
+    draw_shape(shape_side_n)
+
+"""
+from turtle import Turtle, Screen, colormode
+import random
+ANGLE = 360
+
+genie = Turtle()
+colormode(255)
+
+for side in range(3, 11):
+    start_draw = side
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    genie.pencolor(r, g, b)
+    while start_draw > 0:
+        genie.right(ANGLE / side) # use only "/" not "//"
+        genie.forward(100)
+        start_draw -= 1
+  
+screen = Screen()
+screen.exitonclick()
+"""
