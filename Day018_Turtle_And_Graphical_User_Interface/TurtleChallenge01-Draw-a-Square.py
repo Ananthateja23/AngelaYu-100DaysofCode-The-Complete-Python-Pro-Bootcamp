0cd3158f0c470e812f5ecbb7dd09c094af7d6360
AnanthaTeja23 <thatha.ananthateja@gmail.com>
2026-08-12 22:10:03 +0530

    Add TurtleChallenge01 Draw a Square

---
+++ Day018_Turtle_And_Graphical_User_Interface/TurtleChallenge01-Draw-a-Square.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+genie = Turtle()
+
+for _ in range(4):
+    genie.right(90)
+    genie.forward(100)
+
+
+# at the bottom
+screen = Screen()
+screen.exitonclick()
