18132a18b4019041cfbc5371cb53a86cbd0dc6ed
AnanthaTeja23 <thatha.ananthateja@gmail.com>
2026-08-13 20:39:51 +0530

    Update TheHirstPaintingProject Part02 with comments

--- Day018_Turtle_And_Graphical_User_Interface/TheHirstPaintingProject-Drawing-the-Dots.py
+++ Day018_Turtle_And_Graphical_User_Interface/TheHirstPaintingProject-Drawing-the-Dots.py
@@ -1,17 +1,27 @@
+# Import turtle and random module
 import turtle as turtle_module
 import random
 
+# sets the turtle module colormode 
 turtle_module.colormode(255)
+
+# Create a Turtle Object
 tom = turtle_module.Turtle()
+
+# Set the speed, penup, hideturtle functionalities
 tom.speed(speed = "fastest")
 tom.penup()
 tom.hideturtle()
+
 color_list = [(198, 13, 32), (248, 236, 25), (40, 76, 188), (244, 247, 253), (39, 216, 69), (238, 227, 5), (227, 159, 49), (29, 40, 154), (212, 76, 15), (17, 153, 17), (241, 36, 161), (195, 16, 12), (223, 21, 120), (68, 10, 31), (61, 15, 8), (223, 141, 206), (11, 97, 62), (219, 159, 11), (54, 209, 229), (19, 21, 49), (238, 157, 216), (79, 74, 212), (10, 228, 238), (73, 212, 168), (93, 233, 198), (65, 231, 239), (217, 88, 51)]
+
+# logic to change the turtle position
 tom.setheading(225)
 tom.forward(300)
 tom.setheading(0)
 number_of_dots = 100
 
+# logic to draw HirstPainting
 for dot_count in range(1, number_of_dots + 1):
     tom.dot(20, random.choice(color_list))
     tom.forward(50)
@@ -22,8 +32,7 @@ for dot_count in range(1, number_of_dots + 1):
         tom.forward(500)
         tom.setheading(0)
 
-
-
+# Holds the canvas screen
 screen = turtle_module.Screen()
 screen.exitonclick()
 
