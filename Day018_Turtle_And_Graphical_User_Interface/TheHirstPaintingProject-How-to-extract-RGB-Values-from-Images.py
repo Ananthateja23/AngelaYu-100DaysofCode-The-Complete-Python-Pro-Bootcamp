5b6c6c3cb181a10a97bd8d433ff0bea847b5ee67
AnanthaTeja23 <thatha.ananthateja@gmail.com>
2026-08-13 20:35:25 +0530

    Update TheHirstPaintingProject Part01 with comments

--- Day018_Turtle_And_Graphical_User_Interface/TheHirstPaintingProject-How-to-extract-RGB-Values-from-Images.py
+++ Day018_Turtle_And_Graphical_User_Interface/TheHirstPaintingProject-How-to-extract-RGB-Values-from-Images.py
@@ -14,8 +14,14 @@ for color in colors:
 print(rgb_color)
 """
 
+# first install colorgram from PyPI
+# then Import colorgram
 import colorgram
+
+# extracts the most occured colors
 colors = colorgram.extract('Image1.jpg', 30)
+
+# logic to store the colors in a rgb_colors list
 rgb_colors = []
 for color in colors:
     r = color.rgb.r
@@ -25,4 +31,6 @@ for color in colors:
     rgb_colors.append(new_color)
 print(rgb_colors)
 
+
+# After running the code, copy the output and store it on color_list
 color_list = [(198, 13, 32), (248, 236, 25), (40, 76, 188), (244, 247, 253), (39, 216, 69), (238, 227, 5), (227, 159, 49), (29, 40, 154), (212, 76, 15), (17, 153, 17), (241, 36, 161), (195, 16, 12), (223, 21, 120), (68, 10, 31), (61, 15, 8), (223, 141, 206), (11, 97, 62), (219, 159, 11), (54, 209, 229), (19, 21, 49), (238, 157, 216), (79, 74, 212), (10, 228, 238), (73, 212, 168), (93, 233, 198), (65, 231, 239), (217, 88, 51)]
