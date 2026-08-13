ac208da468597ccf83943bd4ee0c444e8b9a7e81
AnanthaTeja23 <thatha.ananthateja@gmail.com>
2026-08-13 20:12:42 +0530

    Update TurtleChallenge01 with comments

--- Day018_Turtle_And_Graphical_User_Interface/TurtleChallenge01-Draw-a-Square.py
+++ Day018_Turtle_And_Graphical_User_Interface/TurtleChallenge01-Draw-a-Square.py
@@ -1,12 +1,15 @@
+# Import turtle module
 from turtle import Turtle, Screen
 
+# Create a Turtle object
 genie = Turtle()
 
+# logic to draw square
 for _ in range(4):
     genie.right(90)
     genie.forward(100)
 
 
-# at the bottom
+# Holds the canvas screen
 screen = Screen()
 screen.exitonclick()
