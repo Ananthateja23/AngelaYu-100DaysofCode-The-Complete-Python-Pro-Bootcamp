e84c785ed737014cb18fd3dff5d8580b1a6f98bc
AnanthaTeja23 <thatha.ananthateja@gmail.com>
2026-08-13 18:17:59 +0530

    Add TheHirstPaintingProject Part01

---
+++ Day018_Turtle_And_Graphical_User_Interface/TheHirstPaintingProject-How-to-extract-RGB-Values-from-Images.py
@@ -0,0 +1,28 @@
+"""
+import colorgram
+
+# Extract 6 colors from an image.
+colors = colorgram.extract('Image.jpg', 10)
+print(colors)
+print(colors[0])
+print(colors[0].rgb)
+print(tuple(colors[0].rgb))
+rgb_color = []
+for color in colors:
+    rgb_color.append(tuple(color.rgb))
+
+print(rgb_color)
+"""
+
+import colorgram
+colors = colorgram.extract('Image1.jpg', 30)
+rgb_colors = []
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+print(rgb_colors)
+
+color_list = [(198, 13, 32), (248, 236, 25), (40, 76, 188), (244, 247, 253), (39, 216, 69), (238, 227, 5), (227, 159, 49), (29, 40, 154), (212, 76, 15), (17, 153, 17), (241, 36, 161), (195, 16, 12), (223, 21, 120), (68, 10, 31), (61, 15, 8), (223, 141, 206), (11, 97, 62), (219, 159, 11), (54, 209, 229), (19, 21, 49), (238, 157, 216), (79, 74, 212), (10, 228, 238), (73, 212, 168), (93, 233, 198), (65, 231, 239), (217, 88, 51)]
