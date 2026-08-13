69ba08ee8c7d3711ea388aaa80e8a0d68fedd1b5
AnanthaTeja23 <thatha.ananthateja@gmail.com>
2026-08-13 18:16:27 +0530

    Add TurtleChallenge05 Draw a Spirograph

---
+++ Day018_Turtle_And_Graphical_User_Interface/TurtleChallenge05-Draw-a-Spirograph.py
@@ -0,0 +1,54 @@
+# method02: Tutorial explained approach
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+tim.speed(speed = "fastest")
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+def draw_spirograph(size_gap):
+    rotations = int(360 / size_gap)
+    for _ in range(rotations):
+        tim.color(random_color())
+        tim.circle(radius = 100)
+        tim.setheading(tim.heading() + size_gap)
+
+draw_spirograph(size_gap = 5)
+
+screen = t.Screen()
+screen.exitonclick()
+
+"""
+# method01: self explored approach
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+tim.speed(speed = "fastest")
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+position = tim.heading()
+while position <= 360:
+    print(position)
+    tim.color(random_color())
+    tim.circle(radius = 100)
+    position += 5
+    tim.setheading(position)
+
+t.Screen().exitonclick()
+
+"""
