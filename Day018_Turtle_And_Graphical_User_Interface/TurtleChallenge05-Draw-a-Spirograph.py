1a59cf1105cc50f4b8b3d3da2d4c24d6ec7078d7
AnanthaTeja23 <thatha.ananthateja@gmail.com>
2026-08-13 20:30:38 +0530

    Update TurtleChallenge05 with comments

--- Day018_Turtle_And_Graphical_User_Interface/TurtleChallenge05-Draw-a-Spirograph.py
+++ Day018_Turtle_And_Graphical_User_Interface/TurtleChallenge05-Draw-a-Spirograph.py
@@ -1,12 +1,17 @@
 # method02: Tutorial explained approach
+# Import turtle and random module
 import turtle as t
 import random
 
+# Create a Turtle object
 tim = t.Turtle()
 
+# Sets turtle speed
 tim.speed(speed = "fastest")
+# set turtle modules color mode
 t.colormode(255)
 
+# generates random color
 def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
@@ -14,6 +19,7 @@ def random_color():
     color = (r, g, b)
     return color
 
+# Draw a spirograph
 def draw_spirograph(size_gap):
     rotations = int(360 / size_gap)
     for _ in range(rotations):
@@ -23,6 +29,7 @@ def draw_spirograph(size_gap):
 
 draw_spirograph(size_gap = 5)
 
+# Hold the canvas screen
 screen = t.Screen()
 screen.exitonclick()
 
