68d64c27c5f387de23541eed229b850cd6b065f1
AnanthaTeja23 <thatha.ananthateja@gmail.com>
2026-08-17 19:29:56 +0530

    Add Detect Collisions with Food py file

---
+++ Day021_Build_the_Snake_Game_part02_Inheritance_and_List_Slicing/02_Detect-Collisions-with-Food.py
@@ -0,0 +1,54 @@
+# step01: Create a snake body
+# step02: move the snake
+# step03: Control the snake
+# step04: detect collision with food
+# step05: create a scoreboard
+
+# Import necessary modules
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+import time
+
+# create screen instance and set screen properties
+screen = Screen()
+screen.setup(width = 600, height = 600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
+# It is very important to disable the animation effect
+screen.tracer(0) # disable the animation
+
+# create a snake instance
+# It creates three segment snake
+snake = Snake()
+
+# create a food instance
+food = Food()
+
+# create a scoreboard instance
+scoreboard = ScoreBoard()
+
+# Event listeners
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# logic to move the snake
+# Note: if any doubt in logic, just rewatch video
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+
+# holds the popped up window
+screen.exitonclick()
