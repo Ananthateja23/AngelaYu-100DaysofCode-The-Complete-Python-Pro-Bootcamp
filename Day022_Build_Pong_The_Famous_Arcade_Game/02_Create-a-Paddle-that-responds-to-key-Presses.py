e6d14430f546b1db9c56cd58cfba8161e7cae2b0
AnanthaTeja23 <thatha.ananthateja@gmail.com>
2026-08-29 17:30:31 +0530

    Add Create a paddle responds to key presses py file

---
+++ Day022_Build_Pong_The_Famous_Arcade_Game/02_Create-a-Paddle-that-responds-to-key-Presses.py
@@ -0,0 +1,49 @@
+# Step01: Create the screen
+# Step02: Create and move a paddle
+# Step03: Create another paddle
+# Step04: Create the ball and make it move
+# Step05: Detect collision with wall and bounce
+# Step06: Detect collision with paddle
+# Step07: Detect when paddle misses
+# Step08: Keep score
+
+# Import the necessary modules
+from turtle import Screen, Turtle
+
+# create a screen instance and set its properties
+screen = Screen()
+screen.setup(width = 800, height = 600)
+screen.bgcolor("black")
+screen.title("My Pong Game")
+screen.tracer(0) # disable the animation
+
+
+# create a paddle instance and set properties
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(x = 350, y = 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(x = paddle.xcor(), y = new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(x = paddle.xcor(), y = new_y)
+
+# logic to move the paddle up and down
+screen.listen()
+screen.onkey(fun = go_up, key ="Up")
+screen.onkey(fun = go_down, key ="Down")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update() # updates the screen manually everytime
+
+# It holds the popped up window screen
+screen.exitonclick()
